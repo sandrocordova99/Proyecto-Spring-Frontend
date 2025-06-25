8f71ae0f6f2af645d7ff44eb0cc8ef56e8a3139d
suusuario <sandrocordova99@hotmail.com>
2025-06-24 22:25:25 -0500

    se agregan componentes de las vistas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { UserComponent } from '../vistas/user/user.component';
+import { AdministradoresComponent } from './vistas/administradores/administradores.component';
+import { AlumnosComponent } from './vistas/alumnos/alumnos.component';
+import { CategoriasComponent } from './vistas/categorias/categorias.component';
+import { CursosComponent } from './vistas/cursos/cursos.component';
+import { GradosComponent } from './vistas/grados/grados.component';
+import { ProfesorComponent } from './vistas/profesor/profesor.component';
+import { SeccionComponent } from './vistas/seccion/seccion.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    UserComponent,
+    AdministradoresComponent,
+    AlumnosComponent,
+    CategoriasComponent,
+    CursosComponent,
+    GradosComponent,
+    ProfesorComponent,
+    SeccionComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    CommonModule
+  ]
 })
 export class AppModule { }
